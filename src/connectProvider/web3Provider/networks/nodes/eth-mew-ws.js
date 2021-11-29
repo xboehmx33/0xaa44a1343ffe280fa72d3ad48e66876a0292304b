f0023b44eb22ceac704fdc4d5ea6a8e9def40d9a
xboehmx33 <94622316+xboehmx33@users.noreply.github.com>
2021-11-29 12:38:01 -0500

    Initialize mobile-increase-timeout

---
+++ src/connectProvider/web3Provider/networks/nodes/eth-mew-ws.js
@@ -0,0 +1,11 @@
+import { ETH } from '../types/index';
+
+export default {
+  type: ETH,
+  service: 'myetherwallet.com-ws',
+  url: 'wss://nodes.mewapi.io/ws/eth',
+  port: 443,
+  auth: false,
+  username: '',
+  password: ''
+};
